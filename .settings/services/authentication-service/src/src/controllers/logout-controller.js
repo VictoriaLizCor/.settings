66d87c1ddf8be26342c48b5a621852d201a5042e
VictoriaLizCor <victoria.lizcor@gmail.com>
2025-03-10 11:41:17 +0100

    [Fix]: Untracking subtree to microservices, adding .settings instead

--- .settings/services/authentication-service/src/src/controllers/logout-controller.js
+++
@@ -1,9 +0,0 @@
-import logoutService from "../services/logout-service.js";
-
-const logoutController = async (request, reply) => {
-	const logoutResult = await logoutService(request.user.id);
-	if (logoutResult.error) {
-    return reply.status(logoutResult.status).send({ error: logoutResult.error });
-	}
-  reply.send({ success: "You have successfully logged out" });
-};
